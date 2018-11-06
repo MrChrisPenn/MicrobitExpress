df8cd816a79416ed027db74fd288e531de36fcd0
Chris Penn <cnpenn@gmail.com>
2018-11-06 20:29:09 +0000

    Add files via upload

---
+++ Train adjustable MC v2.py
@@ -0,0 +1,32 @@
+from microbit import *
+import radio
+
+def stop():
+    pin8.write_analog(1023)
+    pin12.write_analog(1023)
+
+def forward(speed):
+    pin8.write_analog(speed)
+    pin12.write_digital(0)
+    display.scroll(speed)
+
+    
+speed = 0
+
+radio.on()
+while True:
+    incoming = radio.receive()
+    if incoming == 'Forward':
+        msg = str(speed)
+        speed = speed +100
+	display.show(msg)
+        
+        if speed >= 1000:
+            speed = 0
+        #Cycles = 0
+        #while Cycles < 4:
+        forward(speed)
+        
+    if button_b.was_pressed() or incoming == 'Stop':
+        display.scroll('FW stop')
+        stop()
---
+++ train adjustable REMOTEv2.py
@@ -0,0 +1,20 @@
+#remote
+from microbit import *
+import radio
+
+
+radio.on()
+speed = 0
+
+while True:
+    
+    if button_a.was_pressed():
+        radio.send('Forward')
+        display.scroll(str(speed))
+        if speed == 10:
+            speed  = 0
+        
+    
+    if button_b.was_pressed():
+        radio.send('Stop')
+        display.scroll('S')
